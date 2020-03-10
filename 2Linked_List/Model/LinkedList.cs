6fbcc4eda9d77de79769875e977d0266aded3b8a
Хисматулина Карина <hkarinaf@gmail.com>
2020-03-11 00:14:03 +0300

    ну чота намудрила

--- 2Linked_List/Model/LinkedList.cs
+++ 2Linked_List/Model/LinkedList.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace _2Linked_List.Model
 {
-    class LinkedList<T>
+    class LinkedList<T> : IEnumerable
     {
         public Item<T> Head { get; private set; }
         public Item<T> Tail { get; private set; }
@@ -24,6 +25,7 @@ namespace _2Linked_List.Model
         {
             Item<T> item = new Item<T>(data);
             Head = item;
+            Head.Prev = null;
             Tail = item;
             Counter = 1;
         }
@@ -47,12 +49,53 @@ namespace _2Linked_List.Model
                 return;
             }
         }
-
+        
         public void Del(T data)
         {
+            if (Head != null)
+            {
+                var item = new Item<T>(data);
 
+                if (Head.Data.Equals(item))
+                {
+                    Head = Head.Next;
+                    Head.Prev = null;
+                    Counter -= 1;
+                    return;
+                }
+
+                while(Counter-- != 0)
+                {
+                    var curr = Head.Next;
+                    var prev = Head;
+                    while (curr.Next != null)
+                    {
+                        if (curr.Data.Equals(item))
+                        {
+                            prev.Next = curr.Next;
+                            curr = curr.Next;
+                            curr.Prev = prev;
+                            Counter -= 1;
+                        }
+                        else
+                        {
+                            curr = curr.Next;
+                            prev.Next = curr.Prev;
+                        }
+                    }
+                }
+            }
         }
 
+        public IEnumerator GetEnumerator()
+        {
 
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
     }
 }
--- 2Linked_List/Program.cs
+++ 2Linked_List/Program.cs
@@ -5,13 +5,31 @@ namespace _2Linked_List
 {
     class Program
     {
+        static void Print(Model.LinkedList<int> list)
+        {
+            foreach (var n in list)
+            {
+                Console.WriteLine(n + " ");
+            }
+
+            Console.WriteLine(System.Environment.NewLine);
+        }
+
         static void Main(string[] args)
         {
-            Model.LinkedList<int> l1 = new Model.LinkedList<int>();
+
+            Model.LinkedList<int> l1 = new Model.LinkedList<int>(1);
+
+            l1.Add(2);
             l1.Add(3);
-            Console.WriteLine(l1.Head);
-            Console.WriteLine(l1.Tail);
-            Console.ReadLine();
+            l1.Add(4);
+            Print(l1);
+
+            //l1.Del(2);
+            //Print(l1);
+
+            l1.Del(1);
+            Print(l1);
         }
     }
 }
